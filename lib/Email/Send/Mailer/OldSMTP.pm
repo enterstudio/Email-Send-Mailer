dfbf113328fd05dd6b42d0430dcec3eeed6b195f
Ricardo SIGNES <rjbs@cpan.org>
2006-07-16 01:52:12 +0000

    r22040@vex:  rjbs | 2006-07-15 21:52:09 -0400  non-persist smtp
    git-svn-id: file:///Users/rjbs/icg/gitbox/svn/es-mailer/trunk@21883 1bcdbe44-fcfd-0310-b51b-975661d93aa0

---
+++ lib/Email/Send/Mailer/OldSMTP.pm
@@ -0,0 +1,38 @@
+package Email::Send::Mailer::OldSMTP;
+use base qw(Email::Send::Mailer);
+use base qw(Class::Accessor);
+
+use strict;
+use warnings;
+
+use ICG::Handy ();
+
+sub is_available { 1 };
+
+sub new {
+  my ($class, $arg) = @_;
+
+  bless { arg => $arg, } => $class;
+}
+
+sub send {
+  my ($self, $message, $arg) = @_;
+  
+  my @to = ref $arg->{to} ? @{ $arg->{to} } : ($arg->{to});
+
+  eval {
+    ICG::Handy::smtpsend(
+      $message->as_string,
+      to   => \@to,
+      from => $arg->from,
+    );
+  };
+
+  if ($@) {
+    return $self->exception('Email::SendX::Exception::Failure');
+  } else {
+    return $self->exception('Email::SendX::Exception::Success');
+  };
+}
+
+1;
